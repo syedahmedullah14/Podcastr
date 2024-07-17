7291bce953260880aebe08c3853ddb0ab30306fe
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-17 13:17:49 +0530

    half-way done

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ConvexClerkProvider from "./providers/ConvexcClerkProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Podcastr",
+  description: "Generate your podcasts using AI",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}></body>
+      <ConvexClerkProvider>
+        {children}
+      </ConvexClerkProvider>
+    </html>
+  );
+}
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://major-rattler-75.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
