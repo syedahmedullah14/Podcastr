209f1d0dbb4c6c749af05b1c911456c9d8be7c01
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-19 18:42:39 +0530

    Modified PodcastDetailPlayer

--- components/PodcastDetailPlayer.tsx
+++ components/PodcastDetailPlayer.tsx
@@ -5,12 +5,12 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 import { api } from "@/convex/_generated/api";
-import { useAudio } from '@/providers/AudioProvider';
 import { PodcastDetailPlayerProps } from "@/types";
 
 import LoaderSpinner from "./LoaderSpinner";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
+import { useAudio } from "@/app/providers/AudioProvider";
 
 const PodcastDetailPlayer = ({
   audioUrl,
