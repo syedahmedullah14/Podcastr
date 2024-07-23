17bbe733c6b361e2679674f565cb2a5817c93c96
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-23 22:57:55 +0530

    Wrapped app layout in AudioProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClerkProvider from "./providers/ConvexcClerkProvider";
+import AudioProvider from "./providers/AudioProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}></body>
       <ConvexClerkProvider>
+        <AudioProvider>
+
         {children}
+        </AudioProvider>
+
       </ConvexClerkProvider>
     </html>
   );
